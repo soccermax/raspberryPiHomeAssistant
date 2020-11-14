01eb9feecb52cf0709fad29204b197fdc844388c
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2020-11-14 14:54:00 +0100

    SignIn and Dashboard

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+import SignIn from './components/signIn/index';
+import Album from './components/temperatureChart/index';
+import Dashboard from './components/dashboard/Dashboard';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+//TODO: move to env
+firebase.initializeApp({
+    apiKey: "AIzaSyC-_LY36jhUJivQv73hA1Nhe8ITA3lqRhE",
+    authDomain: "raspberrypihomeassistant-ba207.firebaseapp.com",
+    databaseURL: "https://raspberrypihomeassistant-ba207.firebaseio.com",
+    projectId: "raspberrypihomeassistant-ba207",
+    storageBucket: "raspberrypihomeassistant-ba207.appspot.com",
+    messagingSenderId: "775362468335",
+    appId: "1:775362468335:web:abe70711ab522695b7be1a"
+});
+
+const auth = firebase.auth();
+
+function App() {
+    const [user] = useAuthState(auth);
+
+    const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+    return (
+        <div className="App">
+            <ThemeProvider theme={darkTheme}>
+            {user ? <header className='headerChatroom'>
+                <SignOut/>
+            </header> : ""}
+            <section>
+                {user ? <Dashboard/> : <SignIn/>}
+            </section>
+            </ThemeProvider>
+        </div>
+    );
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button className="singOut" onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+export default App;
